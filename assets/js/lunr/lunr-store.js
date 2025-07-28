364485d0c8bfffbd48d094e6eca7217f04f2fd28
Yera10 <Yera10@users.noreply.github.com>
2025-07-28 15:34:29 +0000

    deploy: ec8e69376095d883b6473bcb79d24bc570f34f87

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "/git/git-basic-commands/",
         "teaser": null
       },{
-        "title": "git 기본 명령어",
-        "excerpt":"git 상태 확인하기  git status -s   stage에 올리기  git clone {File_Path}   commit 하기  git commit -m \"커밋메세지\"   push 하기  git push   ","categories": ["git"],
-        "tags": ["test","blog"],
-        "url": "/git/git-basic-commands/",
-        "teaser": null
-      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"이건 첫번째 레슨      좋은 건 너만 알기   이건 두번째 레슨   슬픔도 너만 갖기   $x+y=1$ $x$는 $y$와의 합이 $1$이다.  ","categories": ["coding"],
         "tags": ["test","blog"],
